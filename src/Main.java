e39c51209654e93a912e526ddc4a5b230a37aac4
Aleksandr <graftapisha@gmail.com>
2022-12-03 20:28:41 +0500

    initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 150;
+        // Потраченные рубли
+        int y = 20;
+        // стоимость мили
+        int z = x / y;
+        System.out.println("Начислено миль: " + z);
+    }
+}
